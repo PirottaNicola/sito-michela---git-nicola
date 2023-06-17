84da11b6cccb69ae3c251d918278cd895b245bc6
Nicola Pirotta <nicolapirotta@gmail.com>
2023-06-17 10:44:06 +0200

    first commit

---
+++ provaprogetto.js
@@ -0,0 +1,13 @@
+//* scroll back to top page logic
+//! un casino farlo responsive, forse lascerei perdere
+/* const container_scroll = document.querySelector(".container_scroll");
+container_scroll.addEventListener('scroll', () => {
+ console.log(container_scroll.scrollTop, container_scroll.scrollHeight - container_scroll.clientHeight);
+ if (container_scroll.scrollTop > container_scroll.scrollHeight - container_scroll.clientHeight) {
+  console.log('torna su');
+  container_scroll.scrollTo({
+   top: 0,
+   behavior: "smooth"
+  })
+ }
+}) */
