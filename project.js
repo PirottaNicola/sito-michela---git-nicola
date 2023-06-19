a04265085ae52d73b8466263457979f77244b9e1
Nicola Pirotta <nicolapirotta@gmail.com>
2023-06-19 13:50:25 +0200

    animazioni con scroll snap

---
+++ project.js
@@ -0,0 +1,13 @@
+const aosAnimation = document.querySelectorAll('[data-aos]');
+observer = new IntersectionObserver((entries) => {
+ entries.forEach(entry => {
+  if (entry.intersectionRatio > 0) {
+   entry.target.classList.add('aos-animate');
+  } else {
+   entry.target.classList.remove('aos-animate');
+  }
+ });
+});
+aosAnimation.forEach(aosObject => {
+ observer.observe(aosObject);
+});
